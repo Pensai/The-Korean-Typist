38dbba6db0fc2d018634976b3cad20484df6878a
Pensai <whabanks@gmail.com>
2016-02-21 17:32:44 -0500

    Code restructure, added difficulty options
    Made the code a bit more robust, maintainable and modular. Added the
    medium and hard difficulty lists. And added logic to determine the
    chosen difficulty and display the correct sets of characters

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,59 +1,24 @@
 ﻿$(document).ready(function () {
-    var characterList = ["ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅣ", "ㅏ", "ㅓ", "ㅗ", "ㅎ", "ㄹ", "ㅇ", "ㄴ", "ㅁ", "ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"];
-    // Index for position in characterList array
-    var index = 0;
+    $(characterDisplay).css("font-size", "3em");
+    $(characterDisplay).html("Choose a difficulty Above");
 
-    // Shuffle the characterList array for a "unique" experience each time.
-    shuffle(characterList);
-    $(characterDisplay).html(characterList[index]);
-
-    // onkeypress listener for the textbox which the user uses to match the character
-    $("#input").keypress(function (event)
-    {
-        // if the keypress is "enter".
-        if (event.which == 13) {
-            var typedCharacter = $("#input").val();
-            if (typedCharacter == characterList[index]) {
-                // inform the user it was correct, clear the textbox, and maintain focus on the textbox for them.
-                $("#characterDisplay").animate({ backgroundColor: "green" }, 50).animate({ backgroundColor: jQuery.Color( $(".jumbotron"), "background-color" )}); // animates a green background and then retrieves the background colour of its container and fades it back to its original colour.
-                $("#input").val('');
-                $("#input").focus();
-                index++;
-            }
-            else {
-                $("#characterDisplay").animate({ backgroundColor: "red" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
-                $("#input").val('');
-                $("#input").focus();
-                index++;
-            }
-            setTimeout(displayNextSymbol, 300);
-        }
-    });
-
-    // Click listener for the toggle keyboard anchor tag / button
-    $("#toggleKeyboard").click(function ()
-    {
-        if($("#toggleKeyboard").text() == "Hide Keyboard")
-        {
+    // Click listener for the toggle keyboard link
+    $("#toggleKeyboard").click(function () {
+        if ($("#toggleKeyboard").text() == "Hide Keyboard") {
             $("#koreanKeyboard").fadeOut("slow");
             $("#toggleKeyboard").text("Show Keyboard");
         }
-        else
-        {
+        else {
             $("#koreanKeyboard").fadeIn("slow");
             $("#toggleKeyboard").text("Hide Keyboard");
         }
     }); // toggle keyboard link click listener
-
-    function displayNextSymbol() {
-        $("#characterDisplay").html(characterList[index]);
-    }
 });
 
 /*
  *  Unbiased Fisher-Yates algorithm for shuffling an array and other "list-like" data structures 
  */
-function shuffle(array) {
+function Shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
@@ -68,5 +33,64 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
+
     return array;
 }
+
+function displayNextSymbol(characterList, index)
+{
+    $("#characterDisplay").html(characterList[index]);
+}
+
+function BeginQuiz(difficulty)
+{
+    var EasyCharacterList = ["ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅣ", "ㅏ", "ㅓ", "ㅗ", "ㅎ", "ㄹ", "ㅇ", "ㄴ", "ㅁ", "ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"];
+    var MediumCharacterList = ["가", "구", "나", "무", "두", "모", "다", "누", "오", "모", "게", "르", "두", "개", "내", "사", "리", "네", "부", "라", "머", "로", "소", "부", "시", "마"];
+    var HardCharacterList = ["ㅃ", "ㄲ", "구토", "처녀", "부여하다", "바코드", "여보", "지혜", "캐나다", "키스", "좋아", "십일", "필요하다", "졸업", "좋아", "낙지", "맥주", "맙소사", "있다", "닻줄", "불고기", "짬뽕", "오빠", "예쁘다", "쓰다", "싸다", "빠지다", "비싸다", "깜짝 놀라다", "가깝다", "코끼리", "원숭이", "까다롭다"];
+
+    var CurrentCharacterList = [];
+
+    // Process the selected difficulty
+    if (difficulty == "Easy") {
+        CurrentCharacterList = Shuffle(EasyCharacterList);
+    }
+    else if (difficulty == "Medium") {
+        CurrentCharacterList = Shuffle(MediumCharacterList);
+    }
+    else if (difficulty = "Hard") {
+        CurrentCharacterList = Shuffle(HardCharacterList);
+    }
+
+    // Index for position in characterList array
+    var index = 0;
+
+    // Shuffle the characterList array for a "unique" experience each time.
+    $(characterDisplay).html(CurrentCharacterList[index]);
+
+    // onkeypress listener for the textbox which the user uses to match the character
+    $("#input").keypress(function (event) {
+        // if the keypress is "enter".
+        if (event.which == 13)
+        {
+            // If the typed character matches the displayed character
+            if ($("#input").val() == CurrentCharacterList[index])
+            {
+                // Flash the background colour green to let the user know they got it right
+                $("#characterDisplay").animate({ backgroundColor: "green" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
+                // Clear the input
+                $("#input").val('');
+                // Maintain focus for more seamless typing
+                $("#input").focus();
+                index++;
+            }
+            else
+            {
+                $("#characterDisplay").animate({ backgroundColor: "red" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
+                $("#input").val('');
+                $("#input").focus();
+                index++;
+            }
+            setTimeout(displayNextSymbol(CurrentCharacterList, index), 300);
+        } // event.which == 13
+    });
+} // BeginQuiz()
