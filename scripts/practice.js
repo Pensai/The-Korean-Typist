c2ecd74c53a0a2885a4f6f7896f47f600bf4ab3a
Pensai <whabanks@gmail.com>
2016-02-21 21:24:02 -0500

    Fixed a bug with switching difficulties.
    Fixed a bug where switching difficulties would cause user input to
    always report being incorrect, even if it was correct.

--- scripts/practice.js
+++ scripts/practice.js
@@ -5,14 +5,23 @@
 var AllowTries = true;
 var IsCorrect = false;
 
-$(document).ready(function ()
-{
+var IsInitialDifficulty = false;
+var CurrentIndex = 0;
+var CurrentCharacterList;
+
+var EasyCharacterList = ["ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅣ", "ㅏ", "ㅓ", "ㅗ", "ㅎ", "ㄹ", "ㅇ", "ㄴ", "ㅁ", "ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"];
+var MediumCharacterList = ["가", "구", "나", "무", "두", "모", "다", "누", "오", "모", "게", "르", "두", "개", "내", "사", "리", "네", "부", "라", "머", "로", "소", "부", "시", "마"];
+var HardCharacterList = ["ㅃ", "ㄲ", "구토", "처녀", "부여하다", "바코드", "여보", "지혜", "캐나다", "키스", "좋아", "십일", "필요하다", "졸업", "좋아", "낙지", "맥주", "맙소사", "있다", "닻줄", "불고기", "짬뽕", "오빠", "예쁘다", "쓰다", "싸다", "빠지다", "비싸다", "깜짝 놀라다", "가깝다", "코끼리", "원숭이", "까다롭다"];
+
+$(document).ready(function () {
     $(characterDisplay).css("font-size", "3em");
     $(characterDisplay).html("Choose a difficulty Above");
 
+    $("#AllowTries").attr("class", "form-control btn-success");
+    $("#AllowTries").attr("value", "yes");
+
     // Click listener for the toggle keyboard link
-    $("#toggleKeyboard").click(function ()
-    {
+    $("#toggleKeyboard").click(function () {
         if ($("#toggleKeyboard").text() == "Hide Keyboard") {
             $("#koreanKeyboard").fadeOut("slow");
             $("#toggleKeyboard").text("Show Keyboard");
@@ -23,24 +32,73 @@ $(document).ready(function ()
         }
     }); // toggle keyboard link click listener
 
-    $("#AllowTries").attr("class", "form-control btn-success");
-    $("#AllowTries").attr("value", "yes");
 
-    $("#AllowTries").click(function ()
-    {
+
+    $("#AllowTries").click(function () {
         var value = $("#AllowTries").attr("value");
-        if (value == "yes")
-        {
+        if (value == "yes") {
             $("#AllowTries").attr("value", "no");
             $("#AllowTries").attr("class", "form-control btn-danger");
         }
-        else if (value == "no")
-        {
+        else if (value == "no") {
             $("#AllowTries").attr("value", "yes");
             $("#AllowTries").attr("class", "form-control btn-success");
         }
     });
+
+    $("#input").keypress(function (event) {
+        // if the keypress is "enter".
+        if (event.which == 13) {
+            // If the typed character matches the displayed character
+            if ($("#input").val() == CurrentCharacterList[CurrentIndex]) {
+                // Flash the background colour green to let the user know they got it right
+                $("#characterDisplay").animate({ backgroundColor: "green" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
+                // Clear the input
+                $("#input").val('');
+                // Maintain focus for more seamless typing
+                $("#input").focus();
+                CurrentIndex++;
+            }
+            else {
+                $("#characterDisplay").animate({ backgroundColor: "red" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
+                $("#input").val('');
+                $("#input").focus();
+                CurrentIndex++;
+            }
+
+            if (CurrentIndex == CurrentCharacterList.length) {
+                $("#characterDisplay").html("Well Done! - 참 잘했어요!");
+            }
+            else {
+                setTimeout(displayNextSymbol(CurrentCharacterList, CurrentIndex), 300);
+            }
+        } // event.which == 13
+    });
 });
+
+function displayNextSymbol(characterList, index) {
+    $("#characterDisplay").html(characterList[index]);
+}
+
+function BeginQuiz(difficulty) {
+    switch (difficulty) {
+        case "Easy":
+            CurrentCharacterList = Shuffle(EasyCharacterList);
+            break;
+        case "Medium":
+            CurrentCharacterList = Shuffle(MediumCharacterList);
+            break;
+        case "Hard":
+            CurrentCharacterList = Shuffle(HardCharacterList);
+            break;
+        default:
+            CurrentCharacterList = [1];
+            CurrentCharacterList[0] = "Uh Oh, something went terribly wrong! Feel free to Email me at whabanks@hotmail.com and let me know what you were up to when this happened";
+            break;
+    }
+    // Shuffle the characterList array for a "unique" experience each time.
+    $(characterDisplay).html(CurrentCharacterList[CurrentIndex]);
+} // BeginQuiz()
 
 /*
  *  Unbiased Fisher-Yates algorithm for shuffling an array and other "list-like" data structures 
@@ -62,70 +120,4 @@ function Shuffle(array) {
     }
 
     return array;
-}
-
-function displayNextSymbol(characterList, index)
-{
-    $("#characterDisplay").html(characterList[index]);
-}
-
-function BeginQuiz(difficulty)
-{
-    var EasyCharacterList = ["ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ", "ㅐ", "ㅔ", "ㅣ", "ㅏ", "ㅓ", "ㅗ", "ㅎ", "ㄹ", "ㅇ", "ㄴ", "ㅁ", "ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"];
-    var MediumCharacterList = ["가", "구", "나", "무", "두", "모", "다", "누", "오", "모", "게", "르", "두", "개", "내", "사", "리", "네", "부", "라", "머", "로", "소", "부", "시", "마"];
-    var HardCharacterList = ["ㅃ", "ㄲ", "구토", "처녀", "부여하다", "바코드", "여보", "지혜", "캐나다", "키스", "좋아", "십일", "필요하다", "졸업", "좋아", "낙지", "맥주", "맙소사", "있다", "닻줄", "불고기", "짬뽕", "오빠", "예쁘다", "쓰다", "싸다", "빠지다", "비싸다", "깜짝 놀라다", "가깝다", "코끼리", "원숭이", "까다롭다"];
-
-    var CurrentCharacterList = [];
-
-    // Process the selected difficulty
-    if (difficulty == "Easy") {
-        CurrentCharacterList = Shuffle(EasyCharacterList);
-    }
-    else if (difficulty == "Medium") {
-        CurrentCharacterList = Shuffle(MediumCharacterList);
-    }
-    else if (difficulty = "Hard") {
-        CurrentCharacterList = Shuffle(HardCharacterList);
-    }
-
-    // Index for position in characterList array
-    var index = 0;
-
-    // Shuffle the characterList array for a "unique" experience each time.
-    $(characterDisplay).html(CurrentCharacterList[index]);
-
-    // onkeypress listener for the textbox which the user uses to match the character
-    $("#input").keypress(function (event) {
-        // if the keypress is "enter".
-        if (event.which == 13)
-        {
-            // If the typed character matches the displayed character
-            if ($("#input").val() == CurrentCharacterList[index])
-            {
-                // Flash the background colour green to let the user know they got it right
-                $("#characterDisplay").animate({ backgroundColor: "green" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
-                // Clear the input
-                $("#input").val('');
-                // Maintain focus for more seamless typing
-                $("#input").focus();
-                index++;
-            }
-            else
-            {
-                $("#characterDisplay").animate({ backgroundColor: "red" }, 50).animate({ backgroundColor: jQuery.Color($(".jumbotron"), "background-color") });
-                $("#input").val('');
-                $("#input").focus();
-                index++;
-            }
-
-            if (index == CurrentCharacterList.length)
-            {
-                $("#characterDisplay").html("Well Done / 참 잘했어요");
-            }
-            else
-            {
-                setTimeout(displayNextSymbol(CurrentCharacterList, index), 300);
-            }
-        } // event.which == 13
-    });
-} // BeginQuiz()
+} // Shuffle(array)
